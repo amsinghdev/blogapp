2fe2059c83ed5557c0ec7cd7e19d7b297f1d4509
Amit Singh <amit8829.jnnce@gmail.com>
2022-09-28 09:42:22 +0530

    Exception Handling in spring boot

---
+++ src/main/java/com/amit/blogapp/payload/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.amit.blogapp.payload;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
