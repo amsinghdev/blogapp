5ceced9197cd2f7a9e743d3947ab5ca5caea53f8
Amit Singh <amit8829.jnnce@gmail.com>
2022-10-13 22:13:28 +0530

    DB user authentication

--- src/main/java/com/amit/blogapp/config/SecurityConfig.java
+++ src/main/java/com/amit/blogapp/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.amit.blogapp.config;
 
+import com.amit.blogapp.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +21,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -37,12 +43,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails amit = User.builder().username("amit").password(passwordEncoder().encode("password"))
-                .roles("USER").build();
-        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("password"))
-                .roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(amit,admin);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
+
+    // inmemory user authentication
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails amit = User.builder().username("amit").password(passwordEncoder().encode("password"))
+//                .roles("USER").build();
+//        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("password"))
+//                .roles("ADMIN").build();
+//        return new InMemoryUserDetailsManager(amit,admin);
+//    }
 }
---
+++ src/main/java/com/amit/blogapp/utils/PasswordEncoderGenerater.java
@@ -0,0 +1,12 @@
+package com.amit.blogapp.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncoderGenerater {
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("password"));
+        System.out.println(passwordEncoder.encode("admin"));
+    }
+}
